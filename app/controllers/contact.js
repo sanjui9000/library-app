9e1cf75cab39bad81d02f533ee834d78506e4d67
sanjui9000 <sanjui9000@gmail.com>
2018-04-22 19:47:18 -0700

    First Commit
    1. Created Ember applications foloowing lessons 1 & 2 from "http://yoember.com/".
    
    2. Created .htmlhintrc to make sure DOCTYPE errors are not thrown.
    
    3. Completed the mentioned homeworks.

---
+++ app/controllers/contact.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+
+export default Controller.extend({
+
+  isValidEmail: computed.match('emailAddress', /^.+@.+\..+$/),
+  isMessageEnoughLong: computed.gte('message.length', 5),
+
+  isValid: computed.and('isValidEmail', 'isMessageEnoughLong'),
+
+  actions: {
+    sendMessage: function() {
+      var email = this.get('emailAddress');
+      var message = this.get('message');
+
+      alert('Sending your message in progress... ');
+
+      var responseMessage = 'To: ' + email + ', Message: ' + message;
+      this.set('responseMessage', responseMessage);
+      this.set('emailAddress', '');
+      this.set('message', '');
+    }
+  }
+
+});
